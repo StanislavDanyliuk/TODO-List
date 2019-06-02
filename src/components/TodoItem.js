f77a5d4bc691e070c211c4eb0f9aaf2af25c1be8
Trimar <obivancenubi@gmail.com>
2019-06-03 02:31:38 +0300

    fix

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../main.scss'
 
 const TodoItem = ({ text, isCompleted, deleteTask, id }) => (
     <li className="todo-item collection-item">
         {/* <i className={isCompleted ? 'mark far fa-check-circle' : 'mark far fa-circle'} /> */}
         <span className={isCompleted ? 'completed text' : 'text'}>{text}</span>
-        <i className="material-icons right" onClick={() => deleteTask(id)}>delete</i>
+        <i className="material-icons right red-text delete-btn" onClick={() => deleteTask(id)}>delete</i>
     </li>
 );
 
